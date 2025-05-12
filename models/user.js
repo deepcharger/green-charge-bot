b271224aa612a11f1dd58bf279379e5435205002
deepcharger <github-deep.pp322@slmails.com>
2025-05-12 11:36:17 +0200

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  telegram_id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  first_interaction: {
+    type: Date,
+    default: Date.now
+  },
+  total_charges: {
+    type: Number,
+    default: 0
+  },
+  total_time: {
+    type: Number,
+    default: 0
+  },
+  last_charge: {
+    type: Date,
+    default: null
+  },
+  is_admin: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
