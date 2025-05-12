092d9495efa0eee940263d312c5c480ec297c954
deepcharger <github-deep.pp322@slmails.com>
2025-05-12 11:37:53 +0200

    Update queue.js

--- models/queue.js
+++ models/queue.js
@@ -1,1 +1,23 @@
+const mongoose = require('mongoose');
 
+const queueSchema = new mongoose.Schema({
+  telegram_id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  position: {
+    type: Number,
+    required: true
+  },
+  request_time: {
+    type: Date,
+    default: Date.now
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Queue', queueSchema);
