47d22ac0d62a52b0b70be22037df843b5f08a91c
deepcharger <github-deep.pp322@slmails.com>
2025-05-12 11:37:23 +0200

    Create system.js

---
+++ models/system.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const systemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    default: 'system',
+    unique: true
+  },
+  total_slots: {
+    type: Number,
+    default: 5
+  },
+  slots_available: {
+    type: Number,
+    default: 5
+  },
+  active_sessions: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'Session',
+    default: []
+  },
+  queue_length: {
+    type: Number,
+    default: 0
+  },
+  total_charges_completed: {
+    type: Number,
+    default: 0
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('System', systemSchema);
