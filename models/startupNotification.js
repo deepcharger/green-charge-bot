6ebf909aceb8adf628b6131e0fcaad5c391e7900
deepcharger <github-deep.pp322@slmails.com>
2025-05-13 10:55:51 +0200

    Update startupNotification.js

--- models/startupNotification.js
+++ models/startupNotification.js
@@ -22,7 +22,9 @@ const startupNotificationSchema = new mongoose.Schema({
   timestamps: true 
 });
 
-// Indice TTL per eliminare automaticamente le notifiche vecchie (24 ore)
-startupNotificationSchema.index({ 'createdAt': 1 }, { expireAfterSeconds: 86400 });
+// Indice TTL per eliminare automaticamente le notifiche vecchie (48 ore)
+startupNotificationSchema.index({ 'createdAt': 1 }, { expireAfterSeconds: 172800 });
+startupNotificationSchema.index({ instance_id: 1 });
+startupNotificationSchema.index({ notification_type: 1, timestamp: -1 });
 
 module.exports = mongoose.model('StartupNotification', startupNotificationSchema);
