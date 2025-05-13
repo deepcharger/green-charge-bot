e82cf6dbe24ff53b6fa48fdb0da17c737fa7abaa
deepcharger <github-deep.pp322@slmails.com>
2025-05-13 08:30:15 +0200

    Update lock.js

--- models/lock.js
+++ models/lock.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 
+/**
+ * Schema per i lock del sistema
+ * Supporta sia lock master che lock di esecuzione
+ */
 const lockSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
     unique: true
+  },
+  lock_type: {
+    type: String,
+    enum: ['master', 'execution'],
+    required: true
   },
   instance_id: {
     type: String,
@@ -13,7 +22,7 @@ const lockSchema = new mongoose.Schema({
   created_at: {
     type: Date,
     default: Date.now,
-    expires: 60 // TTL di 60 secondi in caso di crash
+    expires: 300 // TTL di 5 minuti in caso di crash (aumentato per sicurezza)
   },
   last_heartbeat: {
     type: Date,
